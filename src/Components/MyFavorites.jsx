990b44f7da64ba76f47ea9957dd16e8c10317f82
louis-ngugi <louisngugi858@gmail.com>
2025-01-30 00:08:35 +0300

    added const toggleFavorite to MyFavorites.jsx

--- src/Components/MyFavorites.jsx
+++ src/Components/MyFavorites.jsx
@@ -4,7 +4,12 @@ import React, {useState} from "react";
 function MyFavorites() {
   const [favorites, setFavorites] = useState([]);
 
-  
+  const toggleFavorite = (movie) => {
+    if (favorites.includes(movie)) {
+
+    }
+  };
+
   return (
     <div>
         
