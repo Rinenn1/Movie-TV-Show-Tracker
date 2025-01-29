4501fdff04a57e098c9046430bc660ceef52c44f
louis-ngugi <louisngugi858@gmail.com>
2025-01-29 23:04:26 +0300

    Added new features to NavBar+Favorites

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,7 +1,12 @@
 function Navbar() {
   return (
     <div>
-        
+       <nav>
+          <NavLink to="/" className="nav-link">Home</NavLink>
+          <NavLink to="/watchlist" className="nav-link">Watchlist</NavLink>
+          <NavLink to="/favorites" className="nav-link">Favorite</NavLink>
+
+        </nav> 
     </div>
   )
 }
