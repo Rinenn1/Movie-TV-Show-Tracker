1ea020ecce84c75eb9aa310433a3cf5ae1e1e135
louis-ngugi <louisngugi858@gmail.com>
2025-01-30 01:34:43 +0300

    added const movies to MyFavorites.jsx

--- src/Components/MyFavorites.jsx
+++ src/Components/MyFavorites.jsx
@@ -6,9 +6,17 @@ function MyFavorites() {
 
   const toggleFavorite = (movie) => {
     if (favorites.includes(movie)) {
-
+      setFavorites(favorites.filter(fav => fav !== movie));
+    } else {
+      setFavorites([...favorites, movie]);
     }
   };
+
+  const movies = [
+    {id: 1, title:},
+    {id: 2, title:},
+    { id: 3, title:},
+  ];
 
   return (
     <div>
