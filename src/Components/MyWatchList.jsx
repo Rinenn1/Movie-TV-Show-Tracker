6817b114abb4c9351e7130685e2dfc506fdd67d6
Isaac <bennisaac24@gmail.com>
2025-01-30 13:03:05 +0300

    only movie details

--- src/Components/MyWatchList.jsx
+++ src/Components/MyWatchList.jsx
@@ -1,15 +1,7 @@
-import React from "react";
-import MovieList from "../components/MovieList";
-
-const Watchlist = ({ watchlist, removeFromWatchlist }) => {
+const Watchlist = () => {
   return (
     <div>
-      <h2>My Watchlist</h2>
-      {watchlist.length === 0 ? (
-        <p>No movies in your watchlist yet.</p>
-      ) : (
-        <MovieList movies={watchlist} removeFromWatchlist={removeFromWatchlist} watchlist={watchlist} />
-      )}
+      
     </div>
   );
 };
