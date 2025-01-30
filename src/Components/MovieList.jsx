dba589aeb15ddb9e1a503412bd36c0460e55a71e
louis-ngugi <louisngugi858@gmail.com>
2025-01-30 23:44:15 +0300

    Added new changes to App.jsx & MovieList.jsx

--- src/Components/MovieList.jsx
+++ src/Components/MovieList.jsx
@@ -1,9 +1,22 @@
 function MovieList({ movies, onAddToFavorites}) {
   return (
-    <div>
-        
+    <div className="movie-list">
+      <h2>Movie List</h2>
+      <div className="movies-container">
+        {movies.length > 0 ? (
+          movies.map((movie) => (
+            <MovieDetails
+              key={movie.id}
+              movie={movie}
+              onAddToFavorites={onAddToFavorites}
+            />
+          ))
+        ) : (
+          <p>No movies available.</p>
+        )}
+      </div>
     </div>
-  )
+  );
 }
 
-export default MovieList
+export default MovieList;
