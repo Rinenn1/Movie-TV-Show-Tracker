e2f345185709021f43a5e757f7433244efaa61aa
louis-ngugi <louisngugi858@gmail.com>
2025-01-30 14:08:04 +0300

    Added new features to NavBar+Favorites

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,9 +1,18 @@
+import React from "react"
+import App from "./App.jsx"
+import { NavLink } from "react-router-dom"
+
 function Navbar() {
   return (
     <div>
-        
+       <nav>
+          <NavLink to="/" className="nav-link">Movies</NavLink>
+          <NavLink to="/watchlist" className="nav-link">Watchlist</NavLink>
+          <NavLink to="/favorites" className="nav-link">Favorite</NavLink>
+
+        </nav> 
     </div>
   )
 }
 
-export default Navbar
+export default Navbar;
