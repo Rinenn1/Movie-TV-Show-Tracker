ef0089d23e348811aaef973c7578482736219d15
louis-ngugi <louisngugi858@gmail.com>
2025-01-31 00:43:04 +0300

    Added new changes to App.jsx & MovieList.jsx

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import App from "./App"
 import './App.css'
 
 function App() {
@@ -32,3 +33,4 @@ function App() {
   )
 }
 
+export default App
